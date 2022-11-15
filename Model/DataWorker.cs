03073be6cf8b201fbcce24e4941692da6a93c1b0
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-11-15 11:37:40 +0300

    Add project files.

---
+++ Model/DataWorker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageStuffDBApp.Model
+{
+    public static class DataWorker
+    {
+        // создать отдел 
+
+        // создать позицию 
+
+        // создать сотрудника
+
+        // удаление отдела 
+
+        // удаление позиции 
+
+        // удаление сотрудника
+
+        // редактирование отдела 
+
+        // редактирование позиции 
+
+        // редактирование сотрудника
+
+    }
+}
