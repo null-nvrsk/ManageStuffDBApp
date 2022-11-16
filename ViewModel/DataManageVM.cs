774b6431c317aa91f69f37025e83aea531fd8eb9
Maksim Skomorokhov <nullnvrsk@gmail.com>
2022-11-16 17:35:05 +0300

    add Command functionality, bind Add buttons

--- ViewModel/DataManageVM.cs
+++ ViewModel/DataManageVM.cs
@@ -1,12 +1,151 @@
-﻿using System;
+﻿using ManageStuffDBApp.Model;
+using ManageStuffDBApp.View;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ManageStuffDBApp.ViewModel
 {
-    internal class DataManageVM
+    public class DataManageVM : INotifyPropertyChanged
     {
+        // все отделы
+        private List<Department> allDepartments = DataWorker.GetAllDepartments();
+        public List<Department> AllDepartments
+        { 
+            get { return allDepartments; }
+            set 
+            {
+                allDepartments = value;
+                NotifyPropertyChanged("AllDepartments");            
+            }
+        }
+
+        // все должности
+        private List<Position> allPositions = DataWorker.GetAllPositions();
+        public List<Position> AllPositions
+        {
+            get { return allPositions; }
+            set
+            {
+                AllPositions = value;
+                NotifyPropertyChanged("AllPositions");
+            }
+        }
+
+        // все сотрудники
+        private List<Employee> allEmployees = DataWorker.GetAllEmployees();
+        public List<Employee> AllEmployees
+        {
+            get { return allEmployees; }
+            set
+            {
+                allEmployees = value;
+                NotifyPropertyChanged("AllEmployees");
+            }
+        }
+
+        #region COMMANDS TO OPEN WINDOWS
+        private RelayCommand openAddNewDepartmentWnd;
+        public RelayCommand OpenAddNewDepartmentWnd
+        {
+            get
+            {
+                return openAddNewDepartmentWnd ?? new RelayCommand(obj =>
+                {
+                    OpenAddDepartmentWindowMethod();
+                }
+                );
+            }
+        }
+
+        private RelayCommand openAddNewPositionWnd;
+        public RelayCommand OpenAddNewPositionWnd
+        {
+            get
+            {
+                return openAddNewPositionWnd ?? new RelayCommand(obj =>
+                {
+                    OpenAddPositionWindowMethod();
+                }
+                );
+            }
+        }
+
+        private RelayCommand openAddNewEmployeeWnd;
+        public RelayCommand OpenAddNewEmployeeWnd
+        {
+            get
+            {
+                return openAddNewEmployeeWnd ?? new RelayCommand(obj =>
+                {
+                    OpenAddEmployeeWindowMethod();
+                }
+                );
+            }
+        }
+
+        #endregion
+
+        #region METHODS TO OPEN/EDIT WINDOW
+        // Методы открытия окон
+        private void OpenAddDepartmentWindowMethod()
+        {
+           AddNewDepartmentWindow newDepartmentWindow = new AddNewDepartmentWindow();
+            SetCenterPositionAndOpen(newDepartmentWindow);
+        }
+
+        private void OpenAddPositionWindowMethod()
+        {
+            AddNewPositionWindow newPositionWindow = new AddNewPositionWindow();
+            SetCenterPositionAndOpen(newPositionWindow);
+        }
+
+        private void OpenAddEmployeeWindowMethod()
+        {
+            AddNewEmployeeWindow newEmployeeWindow = new AddNewEmployeeWindow();
+            SetCenterPositionAndOpen(newEmployeeWindow);
+        }
+
+        // Окна редактирования
+        private void OpenEditDepartmentWindowMethod()
+        {
+            EditDepartmentWindow editDepartmentWindow = new EditDepartmentWindow();
+            SetCenterPositionAndOpen(editDepartmentWindow);
+        }
+
+        private void OpenEditPositionWindowMethod()
+        {
+            EditPositionWindow editPositionWindow = new EditPositionWindow();
+            SetCenterPositionAndOpen(editPositionWindow);
+        }
+
+        private void OpenEditEmployeeWindowMethod()
+        {
+            EditEmployeeWindow editEmployeeWindow = new EditEmployeeWindow();
+            SetCenterPositionAndOpen(editEmployeeWindow);
+        }
+
+        private void SetCenterPositionAndOpen(Window window)
+        {
+            window.Owner = Application.Current.MainWindow;
+            window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            window.ShowDialog();
+        }
+        #endregion
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
